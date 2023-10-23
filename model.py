9000eee3f748e1686df537e0a168e215c5e1636f
GuidoMario <108852348+GuidoMario@users.noreply.github.com>
2023-10-23 16:54:06 +0700

    Add files via upload

---
+++ model.py
@@ -0,0 +1,27 @@
+# import library
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+# Membuat model data EmployeeModel
+
+
+class EmployeeModel(db.Model):
+    # menentukan nama tabel yaitu Karyawan
+    __tablename__ = "Karyawan"
+
+    # mendefinisikan kolom dalam tabel karyawan
+    id = db.Column(db.Integer, primary_key=True)
+    employee_id = db.Column(db.Integer(), unique=True)
+    name = db.Column(db.String())
+    age = db.Column(db.Integer())
+    position = db.Column(db.String(80))
+
+    def __init__(self, employee_id, name, age, position):
+        self.employee_id = employee_id
+        self.name = name
+        self.age = age
+        self.position = position
+
+    def __repr__(self):
+        return f"{self.name}:{self.employee_id}"
